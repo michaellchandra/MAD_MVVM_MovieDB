d068eae947988837710e40b2aa9a463ab92617b7
michaellchandra <76256177+michaellchandra@users.noreply.github.com>
2021-10-29 14:40:10 +0800

    commit for UTS

---
+++ app/src/main/java/com/project/moviedb/view/fragments/NowPlayingFragment.java
@@ -0,0 +1,110 @@
+package com.project.moviedb.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.moviedb.R;
+import com.project.moviedb.adapter.NowPlayingAdapter;
+import com.project.moviedb.helper.ItemClickSupport;
+import com.project.moviedb.model.NowPlaying;
+import com.project.moviedb.view.activities.NowPlayingActivity;
+import com.project.moviedb.viewmodel.MovieViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NowPlayingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NowPlayingFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NowPlayingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NowPlayingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NowPlayingFragment newInstance(String param1, String param2) {
+        NowPlayingFragment fragment = new NowPlayingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+
+    private RecyclerView rv_now_playing;
+    private MovieViewModel view_model;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_now_playing, container, false);
+
+        rv_now_playing = view.findViewById(R.id.rv_now_playing_fragment);
+        view_model = new ViewModelProvider(getActivity()).get(MovieViewModel.class);
+        view_model.getNowPlaying();
+        view_model.getResultNowPlaying().observe(getActivity(), showNowPlaying);
+
+        return view;
+    }
+
+    private Observer<NowPlaying> showNowPlaying = new Observer<NowPlaying>() {
+        @Override
+        public void onChanged(NowPlaying nowPlaying) {
+            rv_now_playing.setLayoutManager(new LinearLayoutManager(getActivity()));
+            NowPlayingAdapter adapter = new NowPlayingAdapter(getActivity());
+            adapter.setListNowPlaying(nowPlaying.getResults());
+            rv_now_playing.setAdapter(adapter);
+
+            ItemClickSupport.addTo(rv_now_playing).setOnItemLongClickListener(new ItemClickSupport.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClicked(RecyclerView recyclerView, int position, View v) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("movieID", ""+nowPlaying.getResults().get(position).getId());
+                    Navigation.findNavController(v).navigate(R.id.action_nowPlayingFragment);
+                    return false;
+                }
+            });
+
+
+        }
+    };
+}
